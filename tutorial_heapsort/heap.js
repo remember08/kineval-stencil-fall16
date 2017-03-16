6a885c4186cceab22c09049a2da255303f02a8ec
Gang Song <gang.song@intel.com>
2017-03-16 17:59:05 +0800

    Implement heap sort

--- tutorial_heapsort/heap.js
+++ tutorial_heapsort/heap.js
@@ -1,32 +1,88 @@
+var unsorted = new Array(5949,8320,1472,6409,3865,4921,3157,459,5185,797,8465,3911,2575,100,1165,4631,9772,3413,78,319);
+var sorted = new Array();
+document.write("1");
 
-// create empty object 
-minheaper = {}; 
+function create_minheap(){
+	this.element = new Array();
+	this.insert = minheap_insert;
+	this.extract = minheap_extract;
+	this.print = minheap_print;
+}
+
 
 // define insert function for min binary heap
-function minheap_insert(heap, new_element) {
-
-    // STENCIL: implement your min binary heap insert operation
+function minheap_insert(new_element) {
+	//document.write("2");
+	this.element.push(new_element);
+	var new_element_index = this.element.length - 1;
+	while(new_element_index != 0 && this.element[new_element_index] < this.element[Math.floor((new_element_index-1)/2)])
+	{
+		this.element[new_element_index] = this.element[Math.floor((new_element_index-1)/2)];
+		this.element[Math.floor((new_element_index-1)/2)] = new_element;
+		new_element_index = Math.floor((new_element_index-1)/2);
+	}
 }
 
-// assign insert function within minheaper object
-minheaper.insert = minheap_insert;
-/* Note: because the minheap_insert function is an object, we can assign 
-      a reference to the function within the minheap object, which can be called
-      as minheap.insert
-*/
+
 
 // define extract function for min binary heap
-function minheap_extract(heap) {
+function minheap_extract() {
+	//document.write("<p>3</p>");
+	if(this.element.length == 0){return;}
+	sorted.push(this.element[0]);
+	this.element[0] = this.element.pop();
+	var old_element_index = 0;
+	var temp = this.element[0];
+	while(old_element_index*2+1 <= this.element.length-1){
+		if(old_element_index*2+1 == this.element.length-1){
+			if(this.element[old_element_index*2+1] < this.element[old_element_index]){
+				this.element[old_element_index] = this.element[old_element_index*2+1];
+				this.element[old_element_index*2+1] = temp;
+				old_element_index = old_element_index*2+1;
+			}	//swap
+			else{return;}	
+		}	//only have one child
+		else{
+			if(this.element[old_element_index*2+1] < this.element[old_element_index*2+2]){
+				this.element[old_element_index] = this.element[old_element_index*2+1];
+				this.element[old_element_index*2+1] = temp;
+				old_element_index = old_element_index*2+1;		
+			}	//swap
+			else{
+				this.element[old_element_index] = this.element[old_element_index*2+2];
+				this.element[old_element_index*2+2] = temp;
+				old_element_index = old_element_index*2+2;
+			}
+		}	//have two child
+	}	//have child
 
-    // STENCIL: implement your min binary heap extract operation
 }
 
-// assign extract function within minheaper object
-
-    // STENCIL: ensure extract method is within minheaper object
-
-
-
+function minheap_print(heap){
+	var str1 = "<p>" +this.element+ "</p>";
+	document.write(str1);
+}
 
 
 
+
+var str1 = "<p>" +unsorted+ "</p>";
+document.write(str1);
+
+heap = new create_minheap();
+for(var i=0; i<unsorted.length;i++)
+{
+	//document.write("<p>1</p>");
+	heap.insert(unsorted[i]);
+	//document.write("1");
+	heap.print();
+}
+for(var i=0; i<unsorted.length;i++)
+{
+	//document.write("<p>2</p>");
+	heap.extract();
+	heap.print();
+}
+
+var str2 = "<p>" +sorted+ "</p>";
+document.write(str2);
